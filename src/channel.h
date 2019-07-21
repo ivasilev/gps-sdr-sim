739c995d1e6d3e2c7f3f95fd560b80036c00a21e
Ivan Vasilev <ivan.v.vasilev@gmail.com>
2019-07-21 21:48:39 +0300

    Move codegen to channel

--- src/channel.h
+++ src/channel.h
@@ -39,6 +39,7 @@ public:
     void ComputeCodePhase(const Range& rho1, const double dt);
     int GenerateNavMsg(const GpsTime&  g, const int init);
     void Eph2sbf(const Ephemeris& eph, const Ionoutc& ionoutc);
+    void Codegen();
 
 private:
     static unsigned long computeChecksum(unsigned long source, int nib);
